d5ee96b7c7322d675add836619aee193713a289d
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-03-20 17:15:00 +0100

    dernier commit

--- Optimized.py
+++ Optimized.py
@@ -36,31 +36,3 @@ actions_sorted = sorted(list_actions, key=lambda x: x["price"])
 for i in actions_sorted:
     print(i["price"])
 """
-
-best_cost = 0
-best_profit = 0
-best_combo = []
-
-n = len(list_actions)
-count = 0
-total_possibilities = 2**n
-
-for i in range(n + 1):
-    for combo in combinations(list_actions, i):
-        count += 1
-
-        total_cost = sum(action["price"] for action in combo)
-        if total_cost <= max_budget:
-            total_profit = sum(action["profit"] for action in combo)
-            if total_profit > best_profit:
-                best_cost = total_cost
-                best_profit = total_profit
-                best_combo = [action["action"] for action in combo]
-
-                # Affiche les combinaisons théoriques,les combinaisons testées, les valeur du cout, du profit ainsi que la liste avec les meilleures action achetées
-                print(f"Combinaisons théoriques : {total_possibilities}")
-                print(f"Combinaisons testées : {count}")
-                print(
-                    f"Le meilleur profit est de {round(best_profit, 2)}€, pour un cout de {best_cost}€"
-                )
-                print(f"Liste des actions: {best_combo}")
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-tableau = [[3, 1, 4], [1, 5, 9], [2, 6, 5]]
+dp = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 
-for i, index in enumerate(tableau):
-    total_ligne = 0
-    for j in index:
-        total_ligne += j
-    print(f"Ligne: {i} -> {total_ligne}")
+print(dp)
+
+n = 1
+for index, i in enumerate(dp):
+    for j in i:
+        j = n
+
+print(dp)
