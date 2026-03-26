a64216446d7c41137623880fb0f84504662c2318
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-03-26 17:56:31 +0100

    ajout du scirpt glouton

--- Optimized2.py
+++ Optimized2.py
@@ -30,13 +30,34 @@ def load_actions(csv_file):
     return list_actions
 
 
-list_actions = load_actions(file)
+def greedy_actions(list_actions, budget_max):
 
-actions_sorted = sorted(list_actions, key=lambda x: x["rendement"], reverse=True)
+    list_actions_sorted = sorted(
+        list_actions, key=lambda x: x["rendement"], reverse=True
+    )
 
-print(len(actions_sorted))
+    total_cost = 0
+    total_profit = 0
+    actions_selected = []
+
+    for i in list_actions_sorted:
+        if total_cost + i["price"] <= budget_max:
+            total_cost += i["price"]
+            total_profit += i["profit"]
+            actions_selected.append(i["action"])
+
+    return total_profit, total_cost, actions_selected
+
+
+def display_result(best_profit, best_cost, best_combo):
+    print(
+        f"L'achat des {len(best_combo)} actions suivantes:\n"
+        f"{best_combo}\n"
+        f"pour un cout total de {round(best_cost, 2)} EUR a genere {round(best_profit, 2)} EUR de profit"
+    )
+
 
 budget = 500
-n = len(actions_sorted)
-
-test = "Ceci est un test"
+list_actions = load_actions(file)
+total_profit, total_cost, actions_selected = greedy_actions(list_actions, budget)
+display_result(total_profit, total_cost, actions_selected)
--- test.py
+++ test.py
@@ -1,26 +1,17 @@
-actions = [
-    {"name": "A", "cost": 100, "profit": 20},
-    {"name": "B", "cost": 180, "profit": 30},
-    {"name": "C", "cost": 250, "profit": 50},
-    {"name": "D", "cost": 120, "profit": 24},
-    {"name": "E", "cost": 200, "profit": 35},
-]
+def return_money(price):
 
-budget = 500
-n = len(actions)
+    result = []
 
-dp = [[0 for _ in range(budget + 1)] for _ in range(n + 1)]
+    for m in money:
+        while price >= m:
+            price -= m
+            result.append(m)
 
-for a in range(1, n + 1):
-    action = actions[a - 1]
-    price = action["cost"]
-    profit = action["profit"]
-    print(action["name"])
+    return result
 
-    for w in range(budget + 1):
-        if price <= w:
-            dp[a][w] = max(dp[a - 1][w], dp[a - 1][w - price] + profit)
-        else:
-            dp[a][w] = dp[a - 1][w]
 
-print(dp[n][budget])
+price = int(input("Saisir un montant: "))
+money = [50, 20, 10, 5, 2, 1]
+
+result = return_money(price)
+print(result)
